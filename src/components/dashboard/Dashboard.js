1f7d238c557d24964984431c85f936a84b5116d0
Gavin Guo <gavin.guo@canonical.com>
2020-08-28 08:20:42 +0800

    Remove the redundant secondary list
    Signed-off-by: Gavin Guo <gavin.guo@canonical.com>

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -230,8 +230,6 @@ export default function Dashboard() {
         </div>
         <Divider />
         <List>{mainListItems}</List>
-        <Divider />
-        <List>{secondaryListItems}</List>
       </Drawer>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
--- src/components/dashboard/listItems.js
+++ src/components/dashboard/listItems.js
@@ -32,27 +32,3 @@ export const mainListItems = (
     </ListItem>
   </div>
 );
-
-export const secondaryListItems = (
-  <div>
-    <ListSubheader inset>Saved reports</ListSubheader>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Current month" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Last quarter" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Year-end sale" />
-    </ListItem>
-  </div>
-);
