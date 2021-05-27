1b1a8266fba03245dce5d5ad550f6a22beaf0586
Gavin Guo <gavin.guo@canonical.com>
2021-05-27 11:28:35 +0800

    Profile: Make the input/output mechanism complete
    The input/output mechanims is incomplete. The patch add the support
    to make the input/ouput complete and also the error checking.
    
    $ ./profile/cleansing/mem-extract.py   --input-folder ~/ownloads/ --output-folder ~/o
    The output folder /home/vin/o doesn't exist!!
    
    $ ./profile/cleansing/mem-extract.py   --input-folder ~/ownloads/
    The input folder /home/vin/ownloads/ doesn't exist!
    
    $ ./profile/cleansing/mem-extract.py   --meminfo  --output-folder ~/os
    Parsing /var/log/easy-flamegraph/sysinfo/mem-stat/meminfo.log
    The /home/vin/os/meminfo.csv is generated
    
    $ ./profile/cleansing/mem-extract.py   --meminfo ~/Downloads/meminfo.log --output-folder ~/os
    Parsing /home/vin/Downloads/meminfo.log
    The /home/vin/os/meminfo.csv is generated
    
    Signed-off-by: Gavin Guo <gavin.guo@canonical.com>

--- profile/cleansing/mem-extract.py
+++ profile/cleansing/mem-extract.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+import sys
 from utils.parser import parse_to_csv
 
 ez_dir = "/var/log/easy-flamegraph/sysinfo/mem-stat/"
@@ -19,7 +20,7 @@ def initialize_arguments(current_dir):
     parser.add_argument(
         '-o', '--output-folder',
         type=str,
-        help='The output folder with the .csv files',
+        help='The output folder with the .csv files(default: ./)',
         default=current_dir)
     parser.add_argument(
         '-s', '--separate',
@@ -31,6 +32,12 @@ def initialize_arguments(current_dir):
         action='store_true',
         help='To generate the all filetype(meminfo/vmstat...) csv',
         default=False)
+    parser.add_argument(
+        '-i', '--input-folder',
+        type=str,
+        help='The input folder of the captured data'
+             '(default:/var/log/easy-flamegraph/sysinfo/mem-stat',
+        default=argparse.SUPPRESS)
     parser.add_argument(
         '--diff',
         action='store_true',
@@ -51,25 +58,72 @@ def initialize_arguments(current_dir):
     return parser
 
 
-def init_default_paths(current_dir):
+def init_default_paths(args, current_dir):
     ftype_name = {}
+    ftypes = ["meminfo", "vmstat"]
+    output_folder = ""
+    search_folders = [ez_dir, current_dir]
 
-    for ftype in ("meminfo", "vmstat"):
-        if os.path.isfile(os.path.join(current_dir, ftype + '.log')):
-            ftype_name[ftype] = os.path.join(current_dir, ftype + '.log')
-        elif os.path.isfile(os.path.join(ez_dir, ftype + '.log')):
-            ftype_name[ftype] = os.path.join(ez_dir, ftype + '.log')
+    """Set up the output folder"""
+    if hasattr(args, "output_folder"):
+        if os.path.isdir(args.output_folder):
+            output_folder = args.output_folder
         else:
-            ftype_name[ftype] = ""
+            print("The output folder {} doesn't exist!!".format(
+                args.output_folder))
+            sys.exit(1)
 
-    return ftype_name
+    """If the specific file type is assigned, just skip the possible input file
+    source search.
+    """
+
+    if hasattr(args, "meminfo"):
+        if args.meminfo is not None:
+            return ftype_name, output_folder
+    if hasattr(args, "vmstat"):
+        if args.vmstat is not None:
+            return ftype_name, output_folder
+
+    """Set up the default input folder"""
+    if hasattr(args, "input_folder"):
+        search_folders.clear()
+        if os.path.isdir(args.input_folder):
+            search_folders.insert(0, args.input_folder)
+        else:
+            print("The input folder {} doesn't exist!".format(
+                args.input_folder))
+            sys.exit(1)
+
+    for folder in search_folders:
+        for ftype in ftypes:
+            if os.path.isfile(os.path.join(folder, ftype + '.log')):
+                ftype_name[ftype] = os.path.join(folder, ftype + '.log')
+            else:
+                """If any ftype can't find the file, proceed to next folder"""
+                break
+        else:
+            """If the ftype folders loop are all traversed, it means all the
+            ftype files can be found in the folder.
+            """
+            break
+    else:
+        """All the folders are traversed, the ftype cannot find in all
+        folders.
+        """
+        print("Cannot find the valid input file for parsing!!")
+        print("{} don't have all required file type {}.log".format(
+            search_folders, ftypes))
+
+        sys.exit(1)
+
+    return ftype_name, output_folder
 
 
 def main():
     current_dir = os.getcwd()
     parser = initialize_arguments(current_dir)
     args = parser.parse_args()
-    ftype_name = init_default_paths(current_dir)
+    ftype_name, output_folder = init_default_paths(args, current_dir)
     show_usage = True
 
     # (1). Parse the meminfo.log
@@ -77,11 +131,11 @@ def main():
         show_usage = False
         if args.all or args.meminfo is None:
             # The user just input the --meminfo and doesn't have any appends
-            parse_to_csv("meminfo", ftype_name['meminfo'], args.output_folder,
+            parse_to_csv("meminfo", ftype_name['meminfo'], output_folder,
                          args.month, args.separate)
         else:
             # Use the file name provided by the user
-            parse_to_csv("meminfo", args.meminfo, args.output_folder,
+            parse_to_csv("meminfo", args.meminfo, output_folder,
                          args.month, args.separate)
 
     # (2). Parse the vmstat.log
@@ -89,11 +143,11 @@ def main():
         show_usage = False
         if args.all or args.vmstat is None:
             # The user just input the --meminfo and doesn't have any appends
-            parse_to_csv("vmstat", ftype_name['vmstat'], args.output_folder,
+            parse_to_csv("vmstat", ftype_name['vmstat'], output_folder,
                          args.month, args.separate)
         else:
             # Use the file name provided by the user
-            parse_to_csv("vmstat", args.vmstat, args.output_folder,
+            parse_to_csv("vmstat", args.vmstat, output_folder,
                          args.month, args.separate)
 
     if show_usage:
